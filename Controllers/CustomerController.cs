58d4b4919b563598dfc2a04cc7c75ade843ce83b
JigyashaSharma <168462452+JigyashaSharma@users.noreply.github.com>
2024-10-18 17:53:10 +1300

    Task 2: Extend the customer controller to use a dto and not the entity.
    Also Adding the patch method here.

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using IndustryConnect_Week5_WebApi.Models;
+using IndustryConnect_Week5_WebApi.Dtos;
+using IndustryConnect_Week5_WebApi.Mappers;
+using Microsoft.AspNetCore.JsonPatch;
 
 //Task 1: Extend the solution to have a customer controller
 //Used the New Scaffolded Feature to generate this file
@@ -25,14 +28,21 @@ namespace IndustryConnect_Week5_WebApi.Controllers
 
         // GET: api/Customer
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        public async Task<ActionResult<IEnumerable<CustomerDto>>> GetCustomers()
         {
-            return await _context.Customers.ToListAsync();
+            var customersDto = await _context.Customers.Select(c => CustomerMapper.EntityToCustomerDto(c)).ToListAsync();
+
+            if (customersDto.Count > 0)
+            {
+                return Ok(customersDto);
+            }
+            
+            return BadRequest("There are no customer record to display");
         }
 
         // GET: api/Customer/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        public async Task<ActionResult<CustomerDto>> GetCustomer(int id)
         {
             var customer = await _context.Customers.FindAsync(id);
 
@@ -41,18 +51,20 @@ namespace IndustryConnect_Week5_WebApi.Controllers
                 return NotFound();
             }
 
-            return customer;
+            return CustomerMapper.EntityToCustomerDto(customer);
         }
 
         // PUT: api/Customer/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<ActionResult<Customer>> PutCustomer(int id, Customer customer)
+        public async Task<ActionResult<CustomerDto>> PutCustomer(int id, CustomerDto customerDto)
         {
-            if (id != customer.Id)
+            if (id != customerDto.Id)
             {
                 return BadRequest();
             }
+
+            var customer = CustomerMapper.CustomerDtoToEntity(customerDto);
 
             _context.Entry(customer).State = EntityState.Modified;
 
@@ -72,18 +84,21 @@ namespace IndustryConnect_Week5_WebApi.Controllers
                 }
             }
 
-            return customer;
+            return Ok(CustomerMapper.EntityToCustomerDto(customer));
         }
 
         // POST: api/Customer
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+        public async Task<ActionResult<CustomerDto>> PostCustomer(CustomerDto customerDto)
         {
+            var customer = CustomerMapper.CustomerDtoToEntity(customerDto);
+
             _context.Customers.Add(customer);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetCustomer", new { id = customer.Id }, customer);
+            var newCustomerDto = CustomerMapper.EntityToCustomerDto(customer);
+            return CreatedAtAction("GetCustomer", new { id = customer.Id }, newCustomerDto);
         }
 
         // DELETE: api/Customer/5
@@ -102,6 +117,45 @@ namespace IndustryConnect_Week5_WebApi.Controllers
             return $"Customer with ID: {id} deleted successfully!!!";
         }
 
+        [HttpPatch("{id}")]
+        public async Task<ActionResult<CustomerDto>> PatchCustomerDetails(int id, [FromBody] JsonPatchDocument<CustomerDto> patchDto)
+        {
+            try
+            {
+                if (patchDto == null)
+                {
+                    return BadRequest("No values were send to change");
+                }
+
+                var customer = await _context.Customers.FindAsync(id);
+                if (customer == null)
+                {
+                    return BadRequest($"No customer with ID: {id}");
+                }
+
+                var customerDto = CustomerMapper.EntityToCustomerDto(customer);
+                patchDto.ApplyTo(customerDto);
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                _context.Entry(customer).State = EntityState.Detached;
+
+                customer = CustomerMapper.CustomerDtoToEntity(customerDto);
+                _context.Entry(customer).State = EntityState.Modified;
+
+                await _context.SaveChangesAsync();
+
+                return Ok(customerDto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+             
+        }
         private bool CustomerExists(int id)
         {
             return _context.Customers.Any(e => e.Id == id);
